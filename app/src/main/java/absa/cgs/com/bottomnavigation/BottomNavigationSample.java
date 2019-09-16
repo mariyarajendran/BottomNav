4a96c08fb27cb57a3a0d8921551b8297d84c66f7
stcrm-vsts@constient.com <stcrm-vsts@constient.com>
2019-09-16 19:29:03 +0530

    Initial commit

---
+++ app/src/main/java/absa/cgs/com/bottomnavigation/BottomNavigationSample.java
@@ -0,0 +1,15 @@
+package absa.cgs.com.bottomnavigation;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BottomNavigationSample extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom_navigation_sample);
+    }
+}
